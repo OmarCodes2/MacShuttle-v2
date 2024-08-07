f73ee2b1d2a623b9f636b908da69a361da506a59
OmarCodes2 <omarcodes2@gmail.com>
2024-08-07 17:40:45 -0400

    remove unecessary load .env

--- backend/main.go
+++ backend/main.go
@@ -3,28 +3,17 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/OmarCodes2/MacShuttle-v2/handlers"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	// Load .env file
-	err := godotenv.Load()
-	if err != nil {
-		log.Println("Error loading .env file, falling back to environment variables")
-	}
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080" // Default port if PORT is not set
-	}
+	port := "8080"
 
 	http.HandleFunc("/ws", handlers.WebSocketHandler)
 
 	log.Printf("Server started on :%s", port)
-	err = http.ListenAndServe(":"+port, nil)
+	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
