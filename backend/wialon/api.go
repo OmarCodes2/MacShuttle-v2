ecb696c954bcba2cda2f1d60b4c97611aa165dad
Mazen Youssef <98108192+Mazen1004@users.noreply.github.com>
2024-08-18 11:51:01 -0400

    functionaility for multiple buses in api created

--- backend/wialon/api.go
+++ backend/wialon/api.go
@@ -4,64 +4,112 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
-type BusLocation struct {
-	Lat float64 `json:"y"`
-	Lon float64 `json:"x"`
+type Item struct {
+	ID int `json:"id"`
 }
 
-func GetBusLocation() (BusLocation, error) {
-	var location BusLocation
+type TrackerSearchResponse struct {
+	Items []Item `json:"items"`
+}
 
-	// Get environment variables
+type BusData struct {
+	Id int `json:"id"`
+	Lat       float64 `json:"y"`
+	Lon       float64 `json:"x"`
+	TimeStamp float64     `json:"t"`
+}
+
+func GetBusData() ([]BusData, error) {
+	var busesData []BusData
+	var busIDs []int
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	// Get the token from environment variables
 	token := os.Getenv("WIALON_TOKEN")
-	id := os.Getenv("WIALON_ID")
 
 	// Step 1: Get the session ID (sid) using the token
 	resp, err := http.Get(fmt.Sprintf(`https://hst-api.wialon.com/wialon/ajax.html?svc=token/login&params={"token":"%s"}`, token))
 	if err != nil {
-		return location, err
+		return busesData, err
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return location, err
+		return busesData, err
 	}
 
 	var loginResponse map[string]interface{}
 	err = json.Unmarshal(body, &loginResponse)
 	if err != nil {
-		return location, err
+		return busesData, err
 	}
 
 	sid := loginResponse["eid"].(string)
 
-	// Step 2: Get the bus location using the session ID
-	url := fmt.Sprintf(`https://hst-api.wialon.com/wialon/ajax.html?svc=core/search_item&params={"id":%s,"flags":1025}&sid=%s`, id, sid)
-	resp, err = http.Get(url)
+	// Step 2: Get the list of tracker IDs (id) using the session ID (sid)
+	resp, err = http.Get(fmt.Sprintf(`https://hst-api.wialon.com/wialon/ajax.html?svc=core/search_items&params=%%7B%%22spec%%22%%3A%%7B%%22itemsType%%22%%3A%%22avl_unit%%22%%2C%%22propName%%22%%3A%%22sys_name%%22%%2C%%22propValueMask%%22%%3A%%22*%%22%%2C%%22sortType%%22%%3A%%22sys_name%%22%%7D%%2C%%22force%%22%%3A1%%2C%%22flags%%22%%3A1%%2C%%22from%%22%%3A0%%2C%%22to%%22%%3A0%%7D&sid=%s`, sid))
 	if err != nil {
-		return location, err
+		return busesData, err
 	}
 	defer resp.Body.Close()
 
 	body, err = io.ReadAll(resp.Body)
 	if err != nil {
-		return location, err
+		return busesData, err
 	}
 
-	var locationResponse map[string]interface{}
-	err = json.Unmarshal(body, &locationResponse)
+	var trackerSearchResponse TrackerSearchResponse
+	err = json.Unmarshal(body, &trackerSearchResponse)
 	if err != nil {
-		return location, err
+		return busesData, err
 	}
 
-	pos := locationResponse["item"].(map[string]interface{})["pos"].(map[string]interface{})
-	location.Lat = pos["y"].(float64)
-	location.Lon = pos["x"].(float64)
+	for _, item := range trackerSearchResponse.Items {
+		busIDs = append(busIDs, item.ID)
+	}
 
-	return location, nil
+	// Step 3: Get all the bus locations using the session ID & tracker IDs
+	for _, id := range busIDs {
+		var busdata BusData
+		busdata.Id = id
+
+		url := fmt.Sprintf(`https://hst-api.wialon.com/wialon/ajax.html?svc=core/search_item&params={"id":%d,"flags":1025}&sid=%s`, id, sid)
+		resp, err = http.Get(url)
+		if err != nil {
+			return busesData, err
+		}
+		defer resp.Body.Close()
+
+		body, err = io.ReadAll(resp.Body)
+		if err != nil {
+			return busesData, err
+		}
+
+		var locationResponse map[string]interface{}
+		err = json.Unmarshal(body, &locationResponse)
+		if err != nil {
+			return busesData, err
+		}
+
+		pos := locationResponse["item"].(map[string]interface{})["pos"].(map[string]interface{})
+		busdata.Lat = pos["y"].(float64)
+		busdata.Lon = pos["x"].(float64)
+		busdata.TimeStamp = pos["t"].(float64)
+
+		busesData = append(busesData, busdata)
+	}
+	
+	return busesData, nil
 }
